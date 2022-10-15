4621c8b09b7cd382c3492c210bb1bac75644b6f9
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-10-15 15:28:49 -0500

    Creating store.js in src directory and adding code to it using createStore() to create a Redux state store. Also adding code to use middleware with thunk from redux-thunk to have functions which receive a dispatch method and then use it to dispatch synchronous actions after the asynchronous operations are completed. Adding code to App.js importing Provider from react-redux and the state store, and wrapping all components in a <Provider> tag with store.Creating reducers directory and inside alert.js and index.js. Adding code to reducers/index.js importing combineReducers from redux. Adding code to reducers/alert.js setting the initialState as an empty array, and exporting a function with switch-case, to have a reducer to be called depending on the type of action dispatched. Creating actions directory with alert.js and types.js. Adding code to actions/alert.js importing SET_ALERT and REMOVE_ALERT reducers, setting the setAlert() action. Adding code to actions/types.js setting the SET_ALERT and REMOVE_ALERT action types. Adding code to Register.js, importing connect from react-redux and PropTypes, using connect() to relate the Register component to the Redux state store, and prop-types to set validators for setAlert prop given to Register component. Creating layout/Alert.js component, adding code importing connect and PropTypes, defining a mapStateToProps function to map the Redux state to the alerts prop, and also performing a map() array method to return a <div> element for every alert in the alerts array.

--- client/src/App.js
+++ client/src/App.js
@@ -4,33 +4,41 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Routes>
-          <Route 
-            path="/"
-            element={<Landing />}
-          />
-        </Routes>
-        <section className="container">
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Routes>
             <Route 
-              path="/register"
-              element={<Register />}
-            />
-            <Route 
-              path="/login"
-              element={<Login />}
+              path="/"
+              element={<Landing />}
             />
           </Routes>
-        </section>
-      </Fragment>
-    </Router>
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route 
+                path="/register"
+                element={<Register />}
+              />
+              <Route 
+                path="/login"
+                element={<Login />}
+              />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   )
 };
 
