352463ec3bbb3091af9d5061b4294ab8142d3249
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-10-26 22:25:03 -0500

    Adding code to actions/types.js setting LOGOUT type. Adding code to actions/auth.js setting logout() function or action to dispatch type LOGOUT. Adding code to reducers/auth.js importing LOGOUT type and setting case LOGOUT that removes token from localStorage and return the auth state. Modifying code in Navbar.js, importing logout action, connect() from react-redux and PropTypes, setting mapStateToProps() to get the auth state into an auth prop. Then setting auth prop for Navbar, destructuring isAuthenticated and loading, and logout prop. Setting const authLinks to return a <ul> with <li>, <a>, <i> and <span> for a Logout link with an icon. Setting const guestLinks to return a <ul> with <li>, <a> and <Link> for 'Developers', 'Register' and 'Login' links. Modifying const Navbar return, using && and ternaries for conditional rendering of the Navbar links depending on the user being logged in or not.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
     AUTH_ERROR,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    LOGOUT
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -60,7 +61,7 @@ export const register = ({ name, email, password }) => async dispatch => {
             type: REGISTER_FAIL
         });
     }
-}
+};
 
 // Login User
 export const login = (email, password) => async dispatch => {
@@ -92,4 +93,9 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL,
         });
     }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 };
